e1d7ca5c1b86c6dc4e844ebf246be597ba654e7c
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-12 09:24:15 +0300

    Add files via upload

---
+++ Assignment4.m
@@ -0,0 +1,24 @@
+A=[5 2 4; 1  7 -3; 6 -10 0];
+A
+B=[11 5 -3; 0 -12 4; 2 6 1];
+B
+C=[7 14 1; 10 3 -2;8 -5 9];
+C
+%Addition of matrices is commutative that is the order while adding does
+%not matter the same result is acquired
+A+B
+B+A
+%Addition of matrices is associative
+A+(B+C)
+(A+B)+C
+%Multiplication in matrices is distributive 
+5*(A+C)
+5*A+5*C
+%Multiplication in matrices is distributive 
+A*(B+C)
+A*B+A*C
+%Matrix multiplication is not symmetric
+A*B
+B*A
+
+
